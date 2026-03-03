aee12eb35c1f2d3f5404e4fe3bc979e352bd5418
petdomaa100 <petoczdomi@gmail.com>
2026-03-03 16:27:42 +0100

    feat: implemented entire BMS state

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+// [Batter Management System GUI]
+//
+// This is a GUI for a battery management system for the battery of an electric car.
+// The program receives data over UART from the battery pack. The battery consists of 
+// multiple modules, where each module holds multiple battery cells. Each module has
+// multiple temperature sensors at different points in the module.
+// 
+// The program is using Raylib for rendering and a small layout library called Clay
+// for layout management.
+//
+// For testing purposes, we'll simulate receiving UART signals by generating
+// random data every 300ms and parsing it as if it were received over UART.
+// =====
+
 #define CLAY_IMPLEMENTATION
 #include "clay.h"
 #include "raylib/raylib.h"
@@ -5,9 +19,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <float.h>
 
-#define MOCK_VALUES_CNT 5
-#define MAX_STRINGIFIED_NUMBER_LEN 32
+#define MODULES_CNT                4
+#define PCB_CNT                    4
+#define CELL_CNT_PER_MODULE        40
+#define TEMP_SENSOR_CNT_PER_MODULE 12
+
+#define VALUES_TO_GENERATE_CNT (                 \
+    (MODULES_CNT * CELL_CNT_PER_MODULE) +        \
+    (MODULES_CNT * TEMP_SENSOR_CNT_PER_MODULE) + \
+    (PCB_CNT) +                                  \
+    14                                           \
+)                                                // 14x non-array fields in BmsState struct
+
+// This implementation assumes that no stringified float will take up more than 32 characters
+#define MAX_STRINGIFIED_FLOAT_LEN 10
+
+typedef struct {
+	float asFloat;
+	Clay_String asString;
+} FloatAndStr;
+
+typedef struct {
+	FloatAndStr cellVoltages[MODULES_CNT * CELL_CNT_PER_MODULE]; //     Voltages of all cells across all modules
+	FloatAndStr cellTemps[MODULES_CNT * TEMP_SENSOR_CNT_PER_MODULE]; // Temps of all sensors across all modules
+	FloatAndStr pcbTemps[PCB_CNT];
+	FloatAndStr batteryAvgTemp;
+	FloatAndStr humidity;
+	FloatAndStr minTemp;
+	FloatAndStr maxTemp;
+	FloatAndStr avgTemp;
+	FloatAndStr voltageLow;
+	FloatAndStr voltagePack;
+	FloatAndStr currentLow;
+	FloatAndStr currentPack;
+	FloatAndStr minBMSTemp;
+	FloatAndStr maxBMSTemp;
+	FloatAndStr minCellVolt;
+	FloatAndStr maxCellVolt;
+	FloatAndStr stateOfCharge;
+} BmsState;
 
 const Clay_Color COLOR_LIGHT  = (Clay_Color) {224, 215, 210, 255};
 const Clay_Color COLOR_RED    = (Clay_Color) {168,  66,  28, 255};
@@ -15,8 +67,7 @@ const Clay_Color COLOR_ORANGE = (Clay_Color) {225, 138,  50, 255};
 
 typedef struct {
 	Clay_TextElementConfig textConfig;
-	float values[MOCK_VALUES_CNT];
-	Clay_String valuesStr[MOCK_VALUES_CNT];
+	BmsState state;
 } Ctx;
 
 // =====
@@ -35,14 +86,24 @@ void buildGui(Ctx *ctx) {
 				.width  = CLAY_SIZING_GROW(0),
 				.height = CLAY_SIZING_GROW(0),
 			},
-			.layoutDirection = CLAY_LEFT_TO_RIGHT,
-			.childAlignment  = { .x = CLAY_ALIGN_X_CENTER, .y = CLAY_ALIGN_Y_CENTER },
-			.childGap        = 50,
+			.layoutDirection = CLAY_TOP_TO_BOTTOM,
 		},
 		.backgroundColor = COLOR_LIGHT,
 	}) {
-		for (int i = 0; i < MOCK_VALUES_CNT; i++) {
-			CLAY_TEXT(ctx->valuesStr[i], &ctx->textConfig);
+		CLAY_AUTO_ID({
+			.layout = {
+				.sizing = { 
+					.width  = CLAY_SIZING_GROW(0),
+					.height = CLAY_SIZING_GROW(0),
+				},
+				.layoutDirection = CLAY_LEFT_TO_RIGHT,
+				.childAlignment  = { .x = CLAY_ALIGN_X_CENTER, .y = CLAY_ALIGN_Y_CENTER },
+				.childGap        = 25,
+			},
+		}) {
+			for (int i = 0; i < PCB_CNT; i++) {
+				CLAY_TEXT(ctx->state.pcbTemps[i].asString, &ctx->textConfig);
+			}
 		}
 	}
 }
@@ -51,50 +112,107 @@ void buildGui(Ctx *ctx) {
 // UART Signal Processing
 // =====
 
-int generateMockUartSignal(char *dst, size_t dstSize) {
-	assert(dst != NULL || dstSize >= 32);
+static float simulateJitter(float max) {
+	return ((float)rand() / RAND_MAX) * max;
+}
 
-	float values[MOCK_VALUES_CNT];
+void generateMockUartSignal(char *dst, int dstSize) {
+	float values[VALUES_TO_GENERATE_CNT];
+	int idx = 0;
 
-	// Generate some pseudo‑changing values
-	for (int i = 0; i < MOCK_VALUES_CNT; i++) {
-		float jitter = ((float)rand() / RAND_MAX) * 0.1f; // [0..0.1]
-
-		values[i] = (i + 10) + jitter;
+	for (int i = 0; i < MODULES_CNT * CELL_CNT_PER_MODULE; i++) {
+		values[idx++] = 3.7 + simulateJitter(0.5); // Cell voltages
 	}
 
-	// Serialize them 
-	int written = snprintf(
-		dst,
-		dstSize,
-		"%.2f,%.2f,%.2f,%.2f,%.2f",
-		values[0], values[1], values[2], values[3], values[4]
-	);
+	for (int i = 0; i < MODULES_CNT * TEMP_SENSOR_CNT_PER_MODULE; i++) {
+		values[idx++] = 20 + simulateJitter(20); // Cell temps
+	}
 
-	assert(written > 0 && (size_t)written < dstSize);
+	for (int i = 0; i < PCB_CNT; i++) {
+		values[idx++] = 25 + simulateJitter(20); // pcbTemps
+	}
+
+	values[idx++] = 30    + simulateJitter(5);   // batteryAvgTemp
+	values[idx++] = 20    + simulateJitter(60);  // humidity
+	values[idx++] = 20    + simulateJitter(5);   // minTemp
+	values[idx++] = 40    + simulateJitter(10);  // maxTemp
+	values[idx++] = 30    + simulateJitter(5);   // avgTemp
+	values[idx++] = 11.5  + simulateJitter(1.5); // voltageLow
+	values[idx++] = 400   + simulateJitter(20);  // voltagePack
+	values[idx++] = -5    + simulateJitter(10);  // currentLow
+	values[idx++] = -100  + simulateJitter(200); // currentPack
+	values[idx++] = 45    + simulateJitter(10);  // maxBMSTemp
+	values[idx++] = 25    + simulateJitter(5);   // minBMSTemp
+	values[idx++] = 4.15  + simulateJitter(0.1); // maxCellVolt
+	values[idx++] = 3.50  + simulateJitter(0.1); // minCellVolt
+	values[idx++] = 0     + simulateJitter(100); // stateOfCharge
+
+	assert(idx == VALUES_TO_GENERATE_CNT);
+
+	// =====
+
+	int written = 0;
+
+	for (int i = 0; i < idx; i++) {
+		int n = snprintf(
+			dst + written,
+			dstSize - written,
+			(i == idx - 1) ? "%.2f" : "%.2f,",
+			values[i]
+		);
+
+		assert(n >= 0);
+
+		written += n;
+	}
 }
 
 void parseUartData(char *uartBuffer, Ctx *ctx) {
-	assert(uartBuffer != NULL && ctx != NULL);
+	const int CELLS_TOTAL      = MODULES_CNT * CELL_CNT_PER_MODULE;
+	const int TEMPS_TOTAL      = MODULES_CNT * TEMP_SENSOR_CNT_PER_MODULE;
+	const int BASE_SCALARS_IDX = CELLS_TOTAL + TEMPS_TOTAL + PCB_CNT;
 
-	for (int i = 0; i < MOCK_VALUES_CNT; i++) {
-		float value;
-		int charsConsumed = 0;
+	for (int i = 0; i < VALUES_TO_GENERATE_CNT; i++) {
+		// Pointer to the destination field in the BmsState struct where the next parsed value will be stored
+		FloatAndStr *dst;
 
-		if (sscanf(uartBuffer, "%f%n", &value, &charsConsumed) != 1) {
+		if (i < CELLS_TOTAL) {                                         // cellVoltages
+			dst = &ctx->state.cellVoltages[i];
+		} else if (i < CELLS_TOTAL + TEMPS_TOTAL) {                    // cellTemps
+			dst = &ctx->state.cellTemps[i - CELLS_TOTAL];
+		} else if (i < CELLS_TOTAL + TEMPS_TOTAL + PCB_CNT) {          // pcbTemps
+			dst = &ctx->state.pcbTemps[i - CELLS_TOTAL - TEMPS_TOTAL];
+		} else {                                                       // Non-array fields
+			switch (i - BASE_SCALARS_IDX) {
+				case 0:  dst = &ctx->state.batteryAvgTemp; break;
+				case 1:  dst = &ctx->state.humidity;       break;
+				case 2:  dst = &ctx->state.minTemp;        break;
+				case 3:  dst = &ctx->state.maxTemp;        break;
+				case 4:  dst = &ctx->state.avgTemp;        break;
+				case 5:  dst = &ctx->state.voltageLow;     break;
+				case 6:  dst = &ctx->state.voltagePack;    break;
+				case 7:  dst = &ctx->state.currentLow;     break;
+				case 8:  dst = &ctx->state.currentPack;    break;
+				case 9:  dst = &ctx->state.maxBMSTemp;     break;
+				case 10: dst = &ctx->state.minBMSTemp;     break;
+				case 11: dst = &ctx->state.maxCellVolt;    break;
+				case 12: dst = &ctx->state.minCellVolt;    break;
+				case 13: dst = &ctx->state.stateOfCharge;  break;
+				default: assert(0);
+			}
+		}
+
+
+		// =====
+
+		dst->asString.chars = uartBuffer;
+
+		if (sscanf(uartBuffer, "%f%n", &dst->asFloat, &dst->asString.length) != 1) {
 			printf("Failed to parse value #%d\n", i);
 			assert(0);
 		}
 
-		ctx->values[i] = value;
-
-		ctx->valuesStr[i] = (Clay_String){
-			.chars = uartBuffer,
-			.length = charsConsumed,
-			.isStaticallyAllocated = true,
-		};
-
-		uartBuffer += charsConsumed;
+		uartBuffer += dst->asString.length;
 
 		if (*uartBuffer == ',') {
 			uartBuffer++;
@@ -117,23 +235,55 @@ void mockNewUartSignal(char *uartBuffer, size_t uartBufferSize, Ctx *ctx) {
 }
 
 // =====
-// Main entrypoint
-// =====
+
+void printBmsState(BmsState *state) {
+	printf("  batteryAvgTemp: %.2f\n", state->batteryAvgTemp.asFloat);
+	printf("        humidity: %.2f\n", state->humidity.asFloat);
+	printf("         minTemp: %.2f\n", state->minTemp.asFloat);
+	printf("         maxTemp: %.2f\n", state->maxTemp.asFloat);
+	printf("         avgTemp: %.2f\n", state->avgTemp.asFloat);
+	printf("      voltageLow: %.2f\n", state->voltageLow.asFloat);
+	printf("     voltagePack: %.2f\n", state->voltagePack.asFloat);
+	printf("      currentLow: %.2f\n", state->currentLow.asFloat);
+	printf("     currentPack: %.2f\n", state->currentPack.asFloat);
+	printf("      maxBMSTemp: %.2f\n", state->maxBMSTemp.asFloat);
+	printf("      minBMSTemp: %.2f\n", state->minBMSTemp.asFloat);
+	printf("     maxCellVolt: %.2f\n", state->maxCellVolt.asFloat);
+	printf("     minCellVolt: %.2f\n", state->minCellVolt.asFloat);
+	printf("   stateOfCharge: %.2f\n", state->stateOfCharge.asFloat);
+
+	for (int i = 0; i < PCB_CNT; i++) {
+		printf("    pcbTemps[%d]: %.2f\n", i, state->pcbTemps[i].asFloat);
+	}
+
+	for (int i = 0; i < MODULES_CNT * CELL_CNT_PER_MODULE; i++) {
+		printf("cellVoltages[%d]: %.2f\n", i, state->cellVoltages[i].asFloat);
+	}
+
+	for (int i = 0; i < MODULES_CNT * TEMP_SENSOR_CNT_PER_MODULE; i++) {
+		printf("   cellTemps[%d]: %.2f\n", i, state->cellTemps[i].asFloat);
+	}
+
+	printf("\n");
+}
+
+void *safeMalloc(size_t size) {
+	void *ptr = malloc(size);
+
+	assert(ptr != NULL);
+
+	return ptr;
+}
 
 int main() {
 	srand(69420); // Seed
-
-	Ctx ctx;
-	char uartBuffer[256] = {0};
-
-	// =====
 
 	Clay_Raylib_Initialize(900, 600, "Battery Management System GUI", FLAG_WINDOW_RESIZABLE);
 
 	uint64_t arenaSize = Clay_MinMemorySize();
 
 	Clay_Arena clayMemoryArena = {
-		.memory   = malloc(arenaSize),
+		.memory   = safeMalloc(arenaSize),
 		.capacity = arenaSize,
 	};
 
@@ -156,6 +306,13 @@ int main() {
 	Font fonts[1] = { LoadFont("assets/Roboto-Regular.ttf") };
 
 	Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
+
+	Clay_String uartBuffer = (Clay_String){
+		.chars  = safeMalloc(VALUES_TO_GENERATE_CNT * MAX_STRINGIFIED_FLOAT_LEN),
+		.length = VALUES_TO_GENERATE_CNT * MAX_STRINGIFIED_FLOAT_LEN,
+	};
+
+	Ctx ctx = {0};
 
 	ctx.textConfig = (Clay_TextElementConfig) {
 		.fontSize      = 32,
@@ -188,8 +345,8 @@ int main() {
 		// =====
 		// Compute layout & render GUI
 
-		// When live, this would be triggered by UART events instead of mocked every frame
-		mockNewUartSignal(uartBuffer, sizeof(uartBuffer), &ctx);
+		// When live, this would come form the UART stream
+		mockNewUartSignal(uartBuffer.chars, uartBuffer.length, &ctx);
 
 		Clay_BeginLayout();
 		buildGui(&ctx);
@@ -204,6 +361,7 @@ int main() {
 
 	// =====
 
+	free(uartBuffer.chars);
 	free(clayMemoryArena.memory);
 
 	return 0;
